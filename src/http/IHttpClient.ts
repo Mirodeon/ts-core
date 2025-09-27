9c7e1a8e8657465aa2a33aca91e4c5fc5389ea25
Mirodeon <mehdi.habaru@gmail.com>
2025-09-27 18:16:22 +0200

    add missing semicolon

--- src/http/IHttpClient.ts
+++ src/http/IHttpClient.ts
@@ -2,7 +2,7 @@ import {IRequestHttp} from "./IRequestHttp";
 
 export abstract class IHttpClient {
     // @formatter:off
-    abstract Get(request:IRequestHttp): Promise<any>
+    abstract Get(request:IRequestHttp): Promise<any>;
     abstract Post(request:IRequestHttp): Promise<any>;
     abstract Delete(request:IRequestHttp): Promise<any>;
     abstract Patch(request:IRequestHttp): Promise<any>;
